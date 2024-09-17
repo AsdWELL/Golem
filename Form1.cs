f2ab8990320fbd37b0fde6362ce70767a73e8057
AsdWELL <dimagodov.23@mail.ru>
2024-09-17 17:43:03 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace Golem
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Point.cs
@@ -0,0 +1,11 @@
+﻿namespace Golem
+{
+    public class Point(double x, double y, float z)
+    {
+        public double X { get; set; } = x;
+
+        public double Y { get; set; } = y;
+
+        public double Z { get; set; } = z;
+    }
+}
